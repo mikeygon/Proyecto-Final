706f7dee2ba786a44a1e59608e88ec8759563677
Miguel Noroña <mangelnorona@gmail.com>
2022-11-13 17:23:02 -0300

    musica

---
+++ Javascript/Music/songs.js
@@ -0,0 +1,82 @@
+// create Array 
+
+const songs = [
+    {
+        id:'1',
+        songName:` Dfiny Flow <br>
+        <div class="subtitle">Los Aldeanos</div>`,
+        poster: "assets/Music/img/1.jpg"
+    },
+    {
+        id:'2',
+        songName:` Vete <br>
+        <div class="subtitle">Bad Bunny</div>`,
+        poster: "assets/Music/img/2.jpg"
+    },
+    // all object type 
+    {
+        id:"3",
+        songName: `Mission <br><div class="subtitle">La Joaqui</div>`,
+        poster: "assets/Music/img/3.jpg"
+    },
+    {
+        id:"4",
+        songName: `La Bachata <br><div class="subtitle">Manuel Turizo</div>`,
+        poster: "assets/Music/img/4.jpg"
+    },
+    {
+        id:"5",
+        songName: `Session <br><div class="subtitle">Nathy Peluso</div>`,
+        poster: "assets/Music/img/5.jpg"
+    },
+    {
+        id:"6",
+        songName: `Despechao <br><div class="subtitle">Rosalia</div>`,
+        poster: "assets/Music/img/6.jpg"
+    },
+    {
+        id:"7",
+        songName: `Mienteme <br><div class="subtitle">Tini Ft Becerra</div>`,
+        poster: "assets/Music/img/7.jpg"
+    },
+    {
+        id:"8",
+        songName: `Session 51 <br><div class="subtitle">Villano ft Bzrp</div>`,
+        poster: "assets/Music/img/8.jpg"
+    },
+    {
+        id:"9",
+        songName: `Session 52 <br><div class="subtitle">Quevedo Ft Bzrp</div>`,
+        poster: "assets/Music/img/9.jpg"
+    },
+    {
+        id:"10",
+        songName: `Tu Turrito <br><div class="subtitle">Rei</div>`,
+        poster: "assets/Music/img/10.jpg"
+    },
+    {
+        id:"11",
+        songName: `Arrancarmelo <br><div class="subtitle"> WOS</div>`,
+        poster: "assets/Music/img/11.webp"
+    },
+    {
+        id:"12",
+        songName: `Neverita <br><div class="subtitle">Bad Bunny</div>`,
+        poster: "assets/Music/img/12.jpg"
+    },
+    {
+        id:"13",
+        songName: `Ojitos Lindos <br><div class="subtitle">Bad Bunny</div>`,
+        poster: "assets/Music/img/13.jpg"
+    },
+    {
+        id:"14",
+        songName: `A veces <br><div class="subtitle">Al2</div>`,
+        poster: "assets/Music/img/14.jpg"
+    },
+    {
+        id:"15",
+        songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
+        poster: "assets/Music/img/15.jpg"
+    },
+]
